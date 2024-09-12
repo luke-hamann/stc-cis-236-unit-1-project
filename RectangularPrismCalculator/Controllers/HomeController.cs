e3d900dc916ecbfd3ae81ed337ec502615e4448c
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2024-09-12 08:23:17 -0500

    Add calculator form.

---
+++ RectangularPrismCalculator/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RectangularPrismCalculator.Models;
+
+namespace RectangularPrismCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RectangularPrism prism)
+        {
+            return View(prism);
+        }
+    }
+}
