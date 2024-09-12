4f01059c56638583b76c2e515cd3fa87501ebae9
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2024-09-12 12:03:09 -0500

    Add documentation comments.

--- RectangularPrismCalculator/Controllers/HomeController.cs
+++ RectangularPrismCalculator/Controllers/HomeController.cs
@@ -1,6 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RectangularPrismCalculator.Models;
 
+/*
+    Name:    Luke Hamann
+    Date:    2024-09-12
+    Purpose: The HomeController class provides the main controller for the
+             application. It includes action methods for getting a blank copy
+             of the index calculator form, posting to run calculations based on
+             inputs to the index calculator form, and getting the about page.
+*/
 namespace RectangularPrismCalculator.Controllers
 {
     public class HomeController : Controller
